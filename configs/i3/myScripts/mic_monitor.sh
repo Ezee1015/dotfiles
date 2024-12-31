8774bb4fd5b1b7bd9e3b83c6e757a3f48043f05f
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2024-12-31 18:12:18 -0300

    Add microphone check

---
+++ configs/i3/myScripts/mic_monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+MIC_PATH=/proc/asound/card*/pcm*c/sub0/status
+
+if [[ $(grep -HIn "RUNNING" $MIC_PATH) ]]; then
+  APP_NAME=$(pacmd list-source-outputs | grep 'application.process.binary' | sort -u | awk '{print $3}' | tr '\n' ',' | sed -e 's/"//g' -e 's/,$//g' -e 's/,/, /g' -e 's/, \([^,]*\)$/ y \1/')
+  if [[ "$APP_NAME" ]]; then APP_NAME=" $APP_NAME"; fi
+  echo "<span foreground=\"$MIC_ON\">$MIC_ICON$APP_NAME</span>"
+else
+  if [[ -z "$(ls $MIC_PATH)" ]]; then exit 0; fi # No mic available
+  echo "<span foreground=\"$MIC_OFF\">$MIC_ICON</span>"
+fi

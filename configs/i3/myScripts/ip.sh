7c99d461d3e5b1c34969faec12b5922a0a44fb3d
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2025-01-10 18:56:33 -0300

    [i3] Improve ip script

--- configs/i3/myScripts/ip.sh
+++ configs/i3/myScripts/ip.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-IP=$(ip address | grep 192.168 | awk '{print $2}' | sed 's/\(.*\)\/.*/  \1/')
-HIDDEN=$(echo "$IP" | sed 's/[0-9]/*/g')
+# Get the IP and Device used
+INFO=$(ip route get 1)
+# Close if there's no connection
+if [[ -z "$INFO" ]]; then exit 0; fi
+DEVICE=$(echo "$INFO" | awk '{print $(NF-4);exit}')
+IP=$(echo "$INFO" | awk '{print $(NF-2);exit}')
 
-sleep 0.2 # Add delay for dunst to update
-if [[ "$(dunstctl is-paused)" == "false" ]]; then
-  echo "$IP"
+# Get more information about the device
+INFO=$(nmcli connection show --active | grep "$DEVICE")
+TYPE=$(echo "$INFO" | awk '{print $(NF-1);exit}')
+NAME=$(echo "$INFO" | awk '{ for (i=1;i<NF-2;i++) { print $i } }' | tr '\n' ' ')
+NAME=${NAME::-1} # Remove trailing space at the end
+
+# Hide sensitive information
+if [[ "$(dunstctl is-paused)" == "true" ]]; then
+  IP=$(echo "$IP" | sed 's/[0-9]/*/g');
+  NAME=$DEVICE
+fi
+
+if [[ "$TYPE" == "ethernet" ]]; then
+  echo "  (Wired) $IP"
 else
-  echo "$HIDDEN"
+  echo "  ($NAME) $IP"
 fi

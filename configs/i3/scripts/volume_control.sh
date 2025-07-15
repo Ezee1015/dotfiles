6177e825a16abd5b0e4104247db4fb40b782a762
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2025-07-15 12:46:33 -0300

    [i3] Refactor volume control script

--- configs/i3/scripts/volume_control.sh
+++ configs/i3/scripts/volume_control.sh
@@ -3,8 +3,39 @@
 source ~/.config/i3/scripts/refresh_blocks.sh
 source ~/.config/i3/scripts/volume_notification.sh
 
+MUTED=$(amixer sget Master | grep "\[off\]")
+STEP=5
+
+increase() {
+    if [[ -z "$MUTED" ]]; then
+      amixer -q set Master "$STEP%+"
+    else
+      amixer -q set Master "$STEP%"
+      amixer set Master toggle
+    fi
+    refresh_volume_block
+    volume_notification
+}
+
+decrease() {
+    if [[ -z "$MUTED" ]]; then
+      amixer -q set Master "$STEP%-"
+      refresh_volume_block
+    else
+      amixer -q set Master 0%
+      amixer set Master toggle
+    fi
+    volume_notification
+}
+
+mute() {
+    amixer set Master toggle
+    refresh_volume_block
+    volume_notification
+}
+
 error() {
-  echo "You have to provide: increase or decrease"
+  echo "You have to provide: increase, decrease or mute"
   exit 1
 }
 
@@ -12,32 +43,9 @@ if [[ $# -ne 1 ]]; then
   error
 fi
 
-MUTED=$(amixer sget Master | grep "\[off\]")
-
 case "$1" in
-  "increase")
-    if [[ -z "$MUTED" ]]; then
-      amixer -q set Master 5%+
-      refresh_volume_block
-    fi
-    volume_notification
-    ;;
-
-  "decrease")
-    if [[ -z "$MUTED" ]]; then
-      amixer -q set Master 5%-
-      refresh_volume_block
-    fi
-    volume_notification
-    ;;
-
-  "mute")
-    amixer set Master toggle
-    refresh_volume_block
-    volume_notification
-    ;;
-
-  *)
-    error
-    ;;
+  "increase") increase ;;
+  "decrease") decrease ;;
+  "mute")     mute     ;;
+  *)          error    ;;
 esac

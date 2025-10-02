a21430262d813ec6e7866b54b6ffa02c11209f61
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2025-10-02 08:35:18 -0300

    [ranger] Add scope.sh and install odt2txt by default

--- install.sh
+++ install.sh
@@ -191,7 +191,7 @@ install_alacritty() {
 install_ranger() {
   if ! command -v ranger &> /dev/null; then
     echo "- Installing ranger..."
-    sudo apt install ranger exiftool trash-cli
+    sudo apt install ranger exiftool trash-cli odt2txt
   fi
 
   echo "- Linking configuration..."

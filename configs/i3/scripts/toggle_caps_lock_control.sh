9f1bb83d7d5a4cdbb9f87d0237d4c4da54d71d20
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2025-12-06 23:11:16 -0300

    [i3] Add info about remapping alt and super

--- configs/i3/scripts/toggle_caps_lock_control.sh
+++ configs/i3/scripts/toggle_caps_lock_control.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-ENABLED_NOCAPS=$(setxkbmap -query | grep 'ctrl:nocaps')
+REMAP_CONTROL_CAPS_LOCK="ctrl:nocaps"
+# REMAP_SUPER_ALT="altwin:swap_lalt_lwin"
+# REMAP_SUPER_CAPS_LOCK="caps:super"
 
-if [[ -z "$ENABLED_NOCAPS" ]]; then
-  echo "Caps lock as Control: on"
-  setxkbmap -option ctrl:nocaps
+CAPS_FUNCTION="$REMAP_CONTROL_CAPS_LOCK" # ,$REMAP_SUPER_ALT,$REMAP_SUPER_CAPS_LOCK
+
+MAPED=$(setxkbmap -query | grep "$CAPS_FUNCTION")
+if [[ -z $MAPED ]]; then
+  echo "Keys remapped"
+  setxkbmap -option "$CAPS_FUNCTION"
 else
-  echo "Caps lock as Control: off"
+  echo "Keys mapped normal"
   setxkbmap -option
 fi

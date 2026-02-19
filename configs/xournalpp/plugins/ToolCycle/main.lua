355ff15361b0b957800bf0957e764f1c5b8b2fc7
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2026-02-19 20:03:06 -0300

    [xournalpp] Reorder lines of the ToolCycle script

--- configs/xournalpp/plugins/ToolCycle/main.lua
+++ configs/xournalpp/plugins/ToolCycle/main.lua
@@ -20,52 +20,20 @@ function setDefaults()
   app.uiAction({["action"] = "ACTION_TOOL_LINE_STYLE_" .. linestyleList[currentLinestyle]})
 end
 
-function initUi()
-  -- app.registerUi({["menu"] = "Grid snapping on/off",         ["callback"] = "gridsnap",    ["accelerator"] = "<Alt>a"});
-  -- app.registerUi({["menu"] = "Rotation snapping on/off",     ["callback"] = "rotsnap",     ["accelerator"] = "<Alt>o"});
-  app.registerUi({["menu"] = "Fill on/off",                  ["callback"] = "fill",        ["accelerator"] = "f"});
-  app.registerUi({["menu"] = "Tools",                        ["callback"] = "tool",        ["accelerator"] = "q"});
-  app.registerUi({["menu"] = "Solid/Dashed/Dotted",          ["callback"] = "linestyle",   ["accelerator"] = "e"});
-  app.registerUi({["menu"] = "Line/Rectangle/Arrow/Ellipse", ["callback"] = "drawingtype", ["accelerator"] = "w"});
-  app.registerUi({["menu"] = "Select Rectangle/Region",      ["callback"] = "select",      ["accelerator"] = "<Alt>2"});
-
-  -- COLORS SHORTCUTS
-  -- app.registerUi({["menu"] = "Color #1", ["callback"] = "color1", ["accelerator"] = "1"});
-  -- app.registerUi({["menu"] = "Color #2", ["callback"] = "color2", ["accelerator"] = "2"});
-  -- app.registerUi({["menu"] = "Color #3", ["callback"] = "color3", ["accelerator"] = "3"});
-  -- app.registerUi({["menu"] = "Color #4", ["callback"] = "color4", ["accelerator"] = "4"});
-  -- app.registerUi({["menu"] = "Color #5", ["callback"] = "color5", ["accelerator"] = "5"});
-  -- app.registerUi({["menu"] = "Color #6", ["callback"] = "color6", ["accelerator"] = "6"});
-  -- app.registerUi({["menu"] = "Color #7", ["callback"] = "color7", ["accelerator"] = "7"});
-  -- app.registerUi({["menu"] = "Color #8", ["callback"] = "color8", ["accelerator"] = "8"});
-  -- app.registerUi({["menu"] = "Color #9", ["callback"] = "color9", ["accelerator"] = "9"});
-  -- app.registerUi({["menu"] = "Color #0", ["callback"] = "color0", ["accelerator"] = "0"});
-
-  setDefaults()
-end
-
-function gridsnap()
-  currentGridsnap = not currentGridsnap
-  app.uiAction({["action"]="ACTION_GRID_SNAPPING"})
-end
-function rotsnap()
-  currentRotsnap = not currentRotsnap
-  app.uiAction({["action"]="ACTION_ROTATION_SNAPPING"})
-end
-function fill()
-  currentFill = not currentFill
-  app.uiAction({["action"]="ACTION_TOOL_FILL", ["enabled"] = currentFill})
-end
-
-function tool()
-  if (lastClass == "tool") then
-    currentTool = currentTool % #toolList + 1
-  end
-  app.uiAction({["action"] = "ACTION_TOOL_" .. toolList[currentTool]})
-  lastClass = "tool"
-end
-
 -- COLORS SHORTCUTS
+-- function setColorShortcuts()
+--   app.registerUi({["menu"] = "Color #1", ["callback"] = "color1", ["accelerator"] = "1"});
+--   app.registerUi({["menu"] = "Color #2", ["callback"] = "color2", ["accelerator"] = "2"});
+--   app.registerUi({["menu"] = "Color #3", ["callback"] = "color3", ["accelerator"] = "3"});
+--   app.registerUi({["menu"] = "Color #4", ["callback"] = "color4", ["accelerator"] = "4"});
+--   app.registerUi({["menu"] = "Color #5", ["callback"] = "color5", ["accelerator"] = "5"});
+--   app.registerUi({["menu"] = "Color #6", ["callback"] = "color6", ["accelerator"] = "6"});
+--   app.registerUi({["menu"] = "Color #7", ["callback"] = "color7", ["accelerator"] = "7"});
+--   app.registerUi({["menu"] = "Color #8", ["callback"] = "color8", ["accelerator"] = "8"});
+--   app.registerUi({["menu"] = "Color #9", ["callback"] = "color9", ["accelerator"] = "9"});
+--   app.registerUi({["menu"] = "Color #0", ["callback"] = "color0", ["accelerator"] = "0"});
+-- end
+--
 -- local colorList = {
 --   {"white", 0xffffff},
 --   {"gray", 0x808080},
@@ -93,6 +61,41 @@ end
 -- function color8() setColor(8) end
 -- function color9() setColor(9) end
 -- function color0() setColor(10) end
+
+function initUi()
+  -- app.registerUi({["menu"] = "Grid snapping on/off",         ["callback"] = "gridsnap",    ["accelerator"] = "<Alt>a"});
+  -- app.registerUi({["menu"] = "Rotation snapping on/off",     ["callback"] = "rotsnap",     ["accelerator"] = "<Alt>o"});
+  app.registerUi({["menu"] = "Fill on/off",                  ["callback"] = "fill",        ["accelerator"] = "f"});
+  app.registerUi({["menu"] = "Tools",                        ["callback"] = "tool",        ["accelerator"] = "q"});
+  app.registerUi({["menu"] = "Solid/Dashed/Dotted",          ["callback"] = "linestyle",   ["accelerator"] = "e"});
+  app.registerUi({["menu"] = "Line/Rectangle/Arrow/Ellipse", ["callback"] = "drawingtype", ["accelerator"] = "w"});
+  app.registerUi({["menu"] = "Select Rectangle/Region",      ["callback"] = "select",      ["accelerator"] = "<Alt>2"});
+
+  -- setColorShortcuts();
+
+  setDefaults()
+end
+
+function gridsnap()
+  currentGridsnap = not currentGridsnap
+  app.uiAction({["action"]="ACTION_GRID_SNAPPING"})
+end
+function rotsnap()
+  currentRotsnap = not currentRotsnap
+  app.uiAction({["action"]="ACTION_ROTATION_SNAPPING"})
+end
+function fill()
+  currentFill = not currentFill
+  app.uiAction({["action"]="ACTION_TOOL_FILL", ["enabled"] = currentFill})
+end
+
+function tool()
+  if (lastClass == "tool") then
+    currentTool = currentTool % #toolList + 1
+  end
+  app.uiAction({["action"] = "ACTION_TOOL_" .. toolList[currentTool]})
+  lastClass = "tool"
+end
 
 function select()
   if (lastClass == "select") then

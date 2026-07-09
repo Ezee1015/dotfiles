1c3f220dcd16fd1e4aedf6a5d933194bb536bc6d
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2026-07-09 20:06:13 -0300

    [nvim] Change "numToStr/Comment.nvim" to "nvim-mini/mini.comment"

--- configs/nvim/lua/plugins/commentary.lua
+++ configs/nvim/lua/plugins/commentary.lua
@@ -1,11 +1,25 @@
 return {
 	-- Comentar lineas de código
-	{"numToStr/Comment.nvim",
-    opts = {
-      toggler = { -- LHS of toggle mappings in NORMAL mode
-        line = 'gcc', -- Line-comment toggle keymap
-        block = 'gbb', -- Block-comment toggle keymap
-      },
-    }
-  }
+  -- {"numToStr/Comment.nvim",
+  --   opts = {
+  --     toggler = { -- LHS of toggle mappings in NORMAL mode
+  --       line = 'gcc', -- Line-comment toggle keymap
+  --       block = 'gbb', -- Block-comment toggle keymap
+  --     },
+  --   }
+  -- }
+  {
+    'nvim-mini/mini.comment',
+    version = false,
+    config = function()
+      require("mini.comment").setup({
+        mappings = {
+          comment = 'gc',
+          comment_line = 'gcc',
+          comment_visual = 'gc',
+          textobject = 'gc',
+        }
+      })
+    end
+  },
 }
